33a5ed40b1da2bf1af9a46c35ee1a04d14287033
Nicolas Pierson <nicolas.pierson@pm.me>
2025-02-25 10:56:39 +0100

    ✨ Handle post-payment events

--- app.js
+++ app.js
@@ -17,7 +17,17 @@ app.engine(
 app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({}));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith('/webhook')) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
 
 /**
  * Home route
@@ -81,6 +91,51 @@ app.get('/success', function (req, res) {
   res.render('success', {
     stripe_publisable_key: process.env.STRIPE_PUBLISHABLE_KEY,
   });
+});
+
+/**
+ * Expose a endpoint as a webhook handler for asynchronous events.
+ * Configure your webhook in the stripe developer dashboard
+ * https://dashboard.stripe.com/test/webhooks
+ */
+app.post('/webhook', async function (req, res) {
+  let data, eventType;
+
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // we can retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === 'payment_intent.succeeded') {
+    // Funds have been captured
+    // Fulfill any orders, e-mail receipts, etc
+    // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+    console.log('💰 Payment captured!');
+  } else if (eventType === 'payment_intent.payment_failed') {
+    console.log('❌ Payment failed.');
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  res.sendStatus(200);
 });
 
 /**
